d9b90e4f12e09b9c9f785ae5fb538dbd83ed87ae
bill <teebill15@gmail.com>
2023-04-24 23:21:15 +0000

    updated test

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,3 +1,9 @@
 describe('cypress test', () => {
+    beforeEach('route to base url', () => {
+        cy.visit('/')
+    })
+    it('should navigate to conference session page', () => {
+        cy.get('.text-center').should('have.text','SIGN UP TODAY')
+    })
 
 })
