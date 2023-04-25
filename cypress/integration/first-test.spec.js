098db75ddc1f6acb131364a25a7ff72c3b4a931d
bill <teebill15@gmail.com>
2023-04-25 13:56:14 +0000

    added test

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,9 +1,10 @@
-describe('cypress test', () => {
+describe('landing page', () => {
+    // some_text = 'SIGN UP TODAY'
     beforeEach('route to base url', () => {
         cy.visit('/')
     })
-    it('should navigate to conference session page', () => {
-        cy.get('.text-center').should('have.text','SIGN UP TODAY')
+    it('should navigate ', () => {
+        cy.get('.text-center').contains('SIGN UP TODAY')
     })
 
 })
