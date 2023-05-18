3210fecb5692b35c7d7cf6b89c2a9acc3009dc5b
bill <teebill15@gmail.com>
2023-05-18 19:47:00 +0000

    added test

--- cypress/integration/conference.spec.js
+++ cypress/integration/conference.spec.js
@@ -3,14 +3,10 @@ describe('test suite', () => {
         cy.visit('/conference/sessions')
         cy.url().should('include','/sessions')
         cy.viewport('macbook-16')
-        cy.get('[data-test=allsession]').as('SessionBtn')
+        
     })
-    context('route url', () => {
-    it('should route to the url', () =>{
-        cy.intercept('http://localhost:4000/graphql').as('response')
-        })
-    }) 
-    context('start test', () => {
-    
-         
+  
+    it('should route to the page', () => {
+        cy.get('[data-test=Thursday]').should('exist').click()
+    })
 })
