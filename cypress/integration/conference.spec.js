f41f6ffb062d5e67456ba8dc2803792dda30522c
bill <teebill15@gmail.com>
2023-05-21 23:41:08 +0000

    removed aftereach block

--- cypress/integration/conference.spec.js
+++ cypress/integration/conference.spec.js
@@ -5,9 +5,7 @@ describe('test suite', () => {
         cy.viewport('macbook-16')
         
     })
-    afterEach('collapse everything', () => {
-        cy.viewport('iphone-x')
-    })
+    
     context('Thursday Button', () => {
         it('should route to the page', () => {
             cy.intercept('POST','/graphql',{fixture: 'example'}).as('backend')  
