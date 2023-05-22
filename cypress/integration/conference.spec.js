b4ee24a1e58c91fa4bdd00b303085a285e76a6d3
bill <teebill15@gmail.com>
2023-05-22 22:37:46 +0000

    added test

--- cypress/integration/conference.spec.js
+++ cypress/integration/conference.spec.js
@@ -2,7 +2,7 @@ describe('test suite', () => {
     beforeEach('runs this code block', () => {
         cy.visit('/conference/sessions')
         cy.url().should('include','/sessions')
-        cy.viewport('macbook-16')
+        
         
     })
     
