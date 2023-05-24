64c42b18e46f63ff085478d8a4139ba17aa6f59e
bill <teebill15@gmail.com>
2023-05-24 18:16:38 +0000

    added url

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -2,6 +2,7 @@ describe('describe block', () => {
     context('context block', () =>{
         beforeEach('this is before each block', () => {
             cy.viewport(1800,1100)
+            cy.visit('/users')
         })
     })
     context('testing buttons on the UI', () => {
