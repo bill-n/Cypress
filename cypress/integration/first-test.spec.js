8ce630449b34f413982f5edcfced1ee9b7682f4c
bill <teebill15@gmail.com>
2023-05-29 22:26:09 +0000

    added test

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -8,7 +8,7 @@ describe('describe block', () => {
     })
     context('testing buttons on the UI', () => {
         it('should check for login functionality', () => {
-        cy.get("li").should('exist').click()
+        cy.get("li").should('exist')
         
         })
     })
