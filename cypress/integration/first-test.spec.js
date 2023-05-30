fc98ac19d4fa303d2f29df78b066e99923074889
bill <teebill15@gmail.com>
2023-05-30 22:30:06 +0000

    added test

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -3,7 +3,7 @@ describe('describe block', () => {
         beforeEach('this is before each block', () => {
             cy.viewport(1800,1100)
             cy.visit('/users')
-            cy.url()
+            
         })
     })
 })
